907cff579d9c96f8826676b361dd4657c3bda3af
Pablos <pablos@list.ru>
2024-06-05 13:03:02 +0300

    init commit

---
+++ src/main/java/ru/prbank/test_task/seacombat/SeaCombatApplication.java
@@ -0,0 +1,13 @@
+package ru.prbank.test_task.seacombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaCombatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaCombatApplication.class, args);
+    }
+
+}
